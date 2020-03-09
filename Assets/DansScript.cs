9836b7a37e32facf472dffa077fa778fbe49c9e0
ando07 <ando.masahito@gmail.com>
2020-03-10 07:31:31 +0900

    [add]dance start & stop script

---
+++ Assets/DansScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DansScript : MonoBehaviour {
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+
+	public void DanceStart()
+	{
+		anim.enabled = true;
+	}
+
+	public void DanceStop()
+	{
+		anim.enabled = false;
+	}
+}
